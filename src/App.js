6865e95b6b7597074dd4bb1ff40ccfb133360619
NerDiDream <nerdydream@naver.com>
2022-09-25 13:27:19 +0900

    3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Home from './pages/Home';
 import Question from './pages/Question';
 import Result from './pages/Result';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import React, {useState} from 'react'
 import './styles/App.css';
 
 function App() {
   return (
-    <Router>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/question' element={<Question />} />
-            <Route path='/result' element={<Result />} />
-        </Routes>
+    <Router basename="/friendship_quiz/">
+        <Switch>
+            <Route exact path='/home' Component={Home} />
+            <Route exact path='/question' Component={Question} />
+            <Route exact path='/result' Component={Result} />
+        </Switch>
     </Router>
   );
 }
